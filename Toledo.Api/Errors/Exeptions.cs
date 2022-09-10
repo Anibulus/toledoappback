1c418d5374d7d5f26ba441d7751777947cfabfb7
Luis Antonio Preza <anibulusnn@gmail.com>
2022-09-10 16:55:49 -0500

    Fix: Pet mutation

--- Toledo.Api/Errors/Exeptions.cs
+++ Toledo.Api/Errors/Exeptions.cs
@@ -3,7 +3,7 @@ namespace Toledo.Api.Errors;
 public static class Exceptions
 {
     public static readonly string ID_NOT_FOUND = "ID not found";
-    public static readonly string ELEMENT_CANNOT_BE_NULL = "element cannot be null";
+    public static readonly string ELEMENT_CANNOT_BE_NULL = " element cannot be null";
     public static readonly string DNI_DUPLICATED = "DNI is duplicated";
     public static readonly string USER_NOT_FOUND = "User not found";
     public static readonly string INCORRECT_CREDENTIALS = "Incorrect credentials";
