542264792cc7dce33aee640142d648a7b7cf08e3
Luis Antonio Preza <anibulusnn@gmail.com>
2022-09-11 17:44:50 -0500

    Chore: Save default Admin if users entity is empty

--- Toledo.Api/Program.cs
+++ Toledo.Api/Program.cs
@@ -22,4 +22,26 @@ app.MapGet("/", () => "");
 app.MapGet("/liveness", () => "Liveness Toledo");
 app.MapGet("/readiness", () => "Readiness Toledo");
 
+using (var context = new ToledoContext())
+{
+    context.Database.EnsureCreated();
+
+    if(context.Users.Count() == 0)
+    {
+        string? salt = null;
+        string hashedPassword = Toledo.Api.GraphQL.Mutation.UpdatePasswordMutation.GenerateHashPassword("12345678", ref salt);
+        context.Users.Add(new()
+        {
+            Email = "johan@gmail.com",
+            Gender = Toledo.Core.Enumerations.EnumGender.OTHER,
+            Name = "Main User",
+            Role = Toledo.Core.Enumerations.EnumRole.SUPER_ADMIN,
+            Password = hashedPassword,
+            PasswordSalt=(string)salt!
+            
+        });
+        context.SaveChanges();
+    }
+}
+
 await app.RunAsync();
