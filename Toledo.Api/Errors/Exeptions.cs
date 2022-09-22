409598d252c51389ee7188a00339491cb829ffe3
Luis Antonio Preza <anibulusnn@gmail.com>
2022-09-21 21:25:53 -0500

    Rename exceptions

--- Toledo.Api/Errors/Exeptions.cs
+++ Toledo.Api/Errors/Exeptions.cs
@@ -2,11 +2,11 @@ namespace Toledo.Api.Errors;
 
 public static class Exceptions
 {
-    public static readonly string ID_NOT_FOUND = "ID not found";
-    public static readonly string ELEMENT_CANNOT_BE_NULL = " element cannot be null";
-    public static readonly string DNI_DUPLICATED = "DNI is duplicated";
-    public static readonly string USER_NOT_FOUND = "User not found";
-    public static readonly string INCORRECT_CREDENTIALS = "Incorrect credentials";
-    public static readonly string NOT_PERMISSION = "You have not permission to perfom this action";
-    public static readonly string USER_IS_NOT_ACTIVE = "This user is not active";
+    public static readonly string ID_NOT_FOUND = "ID no encontrado";
+    public static readonly string ELEMENT_CANNOT_BE_NULL = " elemento no puede estar vacío";
+    public static readonly string DNI_DUPLICATED = "Documento ya existe";
+    public static readonly string USER_NOT_FOUND = "Usuario no encontrado";
+    public static readonly string INCORRECT_CREDENTIALS = "Credenciales incorrectas";
+    public static readonly string NOT_PERMISSION = "Usted no tiene permiso parra realizar esa acción";
+    public static readonly string USER_IS_NOT_ACTIVE = "Este usuario no esta activo";
 }
